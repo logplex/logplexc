b4801c137e3bc38b449b030f2e903185a9d739de
Daniel Farina <daniel@heroku.com>
2013-01-02 14:37:53 -0800

    Rename Multi to "Client" and Client to "MiniClient"
    The thinking here is that the Minimal Client might be useful to some
    people, but most people are going to want Multi anyway, which is
    capable of batching, dropping, and posting of its own volition.
    
    So, with that thought in mind, rename Multi to be just "Client" so
    that people don't write logplexc consumers that do normally
    undesirable things, such as blocking in event of backlog.
    
    Signed-off-by: Daniel Farina <daniel@heroku.com>

--- logplexc.go
+++ logplexc.go
@@ -1,149 +1,221 @@
+// A client implementation that includes concurrency and dropping.
 package logplexc
 
 import (
-	"bytes"
-	"fmt"
+	"errors"
 	"net/http"
-	"net/url"
-	"strconv"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// Running statistics on Client operation
 type Stats struct {
-	NumberFramed uint64
-	Buffered     int
+	// Number of concurrent requests at the time of retrieval.
+	Concurrency int32
+
+	// Message-level statistics
+
+	// Total messages submitted
+	Total uint64
+
+	// Incremented when a message is ignored outright because of
+	// too much work being done already.
+	Dropped uint64
+
+	// Incremented when a log post request is not known to have
+	// succeeded and one has given up waiting.
+	Cancelled uint64
+
+	// Incremented when a log post request is responded to,
+	// affirming that the messages have been rejected.
+	Rejected uint64
+
+	// Incremented only when a positive response is received from
+	// logplex.
+	Successful uint64
+
+	// Request-level statistics
+
+	TotalRequests   uint64
+	CancelRequests  uint64
+	RejectRequests  uint64
+	SuccessRequests uint64
 }
 
-// Configuration of a Client.
-//
-// The configuration is by-value to prevent most kinds of accidental
-// sharing of modifications between clients.  Also, modification of
-// the URL is compulsary in the constructor, and it's not desirable to
-// modify the version passed by the user as a side effect of
-// constructing a client instance.
-type Config struct {
-	Logplex    url.URL
-	Token      string
-	HttpClient http.Client
-}
-
-// A bundle of messages that are either being accrued to or in the
-// progress of being sent.
-//
-// This is packaged into its own type as it is handy to be able to
-// manipulate bundles to pipeline buffering new logs to be written
-// with bundles that have I/O in progress.
-type bundle struct {
-	Stats
-	outbox bytes.Buffer
-}
-
-// Client context: generally, at a minimum, one should exist per
-// Logplex credential serviced by the program.
 type Client struct {
-	// Configuration that should not be mutated after creation
-	Config
+	Stats
+	statLock sync.Mutex
 
-	reqInFlight sync.WaitGroup
+	c *MiniClient
 
-	// Messages that have been collected but not yet sent.
-	bSwapLock sync.Mutex
-	b         *bundle
+	// Concurrency control of POST workers: the current level of
+	// concurrency, and a token bucket channel.
+	concurrency int32
+	bucket      chan bool
+
+	// Threshold of logplex request size to trigger POST.
+	RequestSizeTrigger int
+
+	// For forcing periodic posting of low-activity logs.
+	ticker *time.Ticker
+
+	// Closed when cleaning up
+	finalize chan bool
 }
 
-func NewClient(cfg *Config) (client *Client, err error) {
-	c := Client{}
+type Config struct {
+	MiniConfig
+	RequestSizeTrigger int
+	Concurrency        int
+	TargetLogLatency   time.Duration
+}
 
-	c.b = &bundle{outbox: bytes.Buffer{}}
-
-	// Make a private copy
-	c.Config = *cfg
-
-	// If the username and password weren't part of the URL, use
-	// the logplex-token as the password
-	if c.Logplex.User == nil {
-		c.Logplex.User = url.UserPassword("token", c.Token)
+func NewClient(cfg *Config) (*Client, error) {
+	c, err := NewMiniClient(&cfg.MiniConfig)
+	if err != nil {
+		return nil, err
 	}
 
-	return &c, nil
-}
+	m := Client{
+		c:                  c,
+		finalize:           make(chan bool),
+		bucket:             make(chan bool),
+		RequestSizeTrigger: cfg.RequestSizeTrigger,
+		ticker:             time.NewTicker(cfg.TargetLogLatency),
+	}
 
-// Unsynchronized statistics gathering function
-//
-// Useful as a subroutine for procedures that already have taken care
-// of synchronization.
-func unsyncStats(b *bundle) Stats {
-	return b.Stats
-}
-
-// Copy the statistics structure embedded in the client.
-func (c *Client) Statistics() Stats {
-	c.bSwapLock.Lock()
-	defer c.bSwapLock.Unlock()
-
-	return unsyncStats(c.b)
-}
-
-// Buffer a message for best-effort delivery to Logplex
-//
-// Return the critical statistics on what has been buffered so far so
-// that the caller can opt to PostMessages() and empty the buffer.
-//
-// No effort is expended to clean up bad bytes disallowed by syslog,
-// as Logplex has a length-prefixed format and the intention that each
-// Client will only process messages for a single user/security
-// context, so at worst it seems a buggy or malicious emitter of logs
-// can cause problems for themselves only.
-func (c *Client) BufferMessage(when time.Time, procId string, log []byte) Stats {
-	// Avoid racing against other operations that may want to swap
-	// out client's current bundle.
-	c.bSwapLock.Lock()
-	defer c.bSwapLock.Unlock()
-
-	ts := when.UTC().Format(time.RFC3339)
-	syslogPrefix := "<134>1 " + ts + " 1234 " +
-		c.Token + " " + procId + " - - "
-	msgLen := len(syslogPrefix) + len(log)
-
-	fmt.Fprintf(&c.b.outbox, "%d %s%s", msgLen, syslogPrefix, log)
-	c.b.NumberFramed += 1
-	c.b.Buffered = c.b.outbox.Len()
-
-	return unsyncStats(c.b)
-}
-
-// Post messages that are pending being posted.
-func (c *Client) PostMessages() (*http.Response, Stats, error) {
-	// Swap out the bundle that is about to go through a long I/O
-	// operation for a fresh one, so that buffering can continue
-	// again immediately.
-	b := func() bundle {
-		c.bSwapLock.Lock()
-		defer c.bSwapLock.Unlock()
-
-		b := *c.b
-		c.b.outbox = bytes.Buffer{}
-		c.b.Stats = Stats{}
-
-		return b
+	// Supply tokens to the buckets.
+	//
+	// This goroutine exits when it has supplied all of the
+	// initial tokens: that's because worker goroutines are
+	// responsible for re-inserting tokens.
+	go func() {
+		for i := 0; i < cfg.Concurrency; i += 1 {
+			m.bucket <- true
+		}
 	}()
 
-	// Record that a request is in progress so that a clean
-	// shutdown can wait for it to complete.
-	c.reqInFlight.Add(1)
-	defer c.reqInFlight.Done()
+	// Periodic log-sending ticker for responsive low-volume
+	// logging.
+	go func() {
+		for {
+			// Wait for a while to do work, or to exit
+			select {
+			case <-m.ticker.C:
+			case _, _ = <-m.finalize:
+				return
+			}
 
-	req, _ := http.NewRequest("POST", c.Logplex.String(), &b.outbox)
-	req.Header.Add("Content-Type", "application/logplex-1")
-	req.Header.Add("Logplex-Msg-Count",
-		strconv.FormatUint(b.NumberFramed, 10))
+			// Avoid sending empty requests
+			s := m.c.Statistics()
+			if s.NumberFramed > 0 {
+				go m.syncWorker()
+			}
+		}
+	}()
 
-	resp, err := c.HttpClient.Do(req)
-	if err != nil {
-		return nil, b.Stats, err
+	return &m, nil
+}
+
+func (m *Client) Close() {
+	// Clean up otherwise immortal ticker goroutine
+	m.ticker.Stop()
+	close(m.finalize)
+}
+
+func (m *Client) BufferMessage(
+	when time.Time, procId string, log []byte) error {
+
+	select {
+	case _, _ = <-m.finalize:
+		return errors.New("Failed trying to buffer a message: " +
+			"client already Closed")
+	default:
+		// no-op
 	}
 
-	return resp, b.Stats, nil
+	m.statLock.Lock()
+	defer m.statLock.Unlock()
+
+	s := m.c.BufferMessage(when, procId, log)
+	if s.Buffered >= m.RequestSizeTrigger {
+		go m.syncWorker()
+	}
+
+	return nil
+}
+
+func (m *Client) Statistics() (s Stats) {
+	m.statLock.Lock()
+	defer m.statLock.Unlock()
+
+	s = m.Stats
+	return s
+}
+
+func (m *Client) syncWorker() {
+	atomic.AddInt32(&m.Stats.Concurrency, 1)
+	defer atomic.AddInt32(&m.Stats.Concurrency, -1)
+
+	// Check if there are any worker tokens available. If not,
+	// then signal someone else to do the work and exit.
+	select {
+	case <-m.bucket:
+		// When exiting, free up the token for use by another
+		// worker.
+		defer func() {
+			m.bucket <- true
+		}()
+	default:
+		return
+	}
+
+	// Post to logplex.
+	resp, s, err := m.c.PostMessages()
+	if err != nil {
+		m.statReqErr(&s)
+	}
+
+	defer resp.Body.Close()
+
+	// Check HTTP return code and accrue statistics accordingly.
+	if resp.StatusCode != http.StatusNoContent {
+		m.statReqRej(&s)
+	} else {
+		m.statReqSuccess(&s)
+	}
+
+	return
+}
+
+func (m *Client) statReqTotalUnsync(s *MiniStats) {
+	m.Total += s.NumberFramed
+	m.TotalRequests += 1
+}
+
+func (m *Client) statReqSuccess(s *MiniStats) {
+	m.statLock.Lock()
+	defer m.statLock.Unlock()
+	m.statReqTotalUnsync(s)
+
+	m.Successful += s.NumberFramed
+	m.SuccessRequests += 1
+}
+
+func (m *Client) statReqErr(s *MiniStats) {
+	m.statLock.Lock()
+	defer m.statLock.Unlock()
+	m.statReqTotalUnsync(s)
+
+	m.Cancelled += s.NumberFramed
+	m.CancelRequests += 1
+}
+
+func (m *Client) statReqRej(s *MiniStats) {
+	m.statLock.Lock()
+	defer m.statLock.Unlock()
+	m.statReqTotalUnsync(s)
+
+	m.Rejected += s.NumberFramed
+	m.RejectRequests += 1
 }
---
+++ minimal.go
@@ -0,0 +1,150 @@
+package logplexc
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+)
+
+// Running statistics on Client operation
+type MiniStats struct {
+	NumberFramed uint64
+	Buffered     int
+}
+
+// Configuration of a Client.
+//
+// The configuration is by-value to prevent most kinds of accidental
+// sharing of modifications between clients.  Also, modification of
+// the URL is compulsary in the constructor, and it's not desirable to
+// modify the version passed by the user as a side effect of
+// constructing a client instance.
+type MiniConfig struct {
+	Logplex    url.URL
+	Token      string
+	HttpClient http.Client
+}
+
+// A bundle of messages that are either being accrued to or in the
+// progress of being sent.
+//
+// This is packaged into its own type as it is handy to be able to
+// manipulate bundles to pipeline buffering new logs to be written
+// with bundles that have I/O in progress.
+type bundle struct {
+	MiniStats
+	outbox bytes.Buffer
+}
+
+// Client context: generally, at a minimum, one should exist per
+// Logplex credential serviced by the program.
+type MiniClient struct {
+	// Configuration that should not be mutated after creation
+	MiniConfig
+
+	reqInFlight sync.WaitGroup
+
+	// Messages that have been collected but not yet sent.
+	bSwapLock sync.Mutex
+	b         *bundle
+}
+
+func NewMiniClient(cfg *MiniConfig) (client *MiniClient, err error) {
+	c := MiniClient{}
+
+	c.b = &bundle{outbox: bytes.Buffer{}}
+
+	// Make a private copy
+	c.MiniConfig = *cfg
+
+	// If the username and password weren't part of the URL, use
+	// the logplex-token as the password
+	if c.Logplex.User == nil {
+		c.Logplex.User = url.UserPassword("token", c.Token)
+	}
+
+	return &c, nil
+}
+
+// Unsynchronized statistics gathering function
+//
+// Useful as a subroutine for procedures that already have taken care
+// of synchronization.
+func unsyncStats(b *bundle) MiniStats {
+	return b.MiniStats
+}
+
+// Copy the statistics structure embedded in the client.
+func (c *MiniClient) Statistics() MiniStats {
+	c.bSwapLock.Lock()
+	defer c.bSwapLock.Unlock()
+
+	return unsyncStats(c.b)
+}
+
+// Buffer a message for best-effort delivery to Logplex
+//
+// Return the critical statistics on what has been buffered so far so
+// that the caller can opt to PostMessages() and empty the buffer.
+//
+// No effort is expended to clean up bad bytes disallowed by syslog,
+// as Logplex has a length-prefixed format and the intention that each
+// Client will only process messages for a single user/security
+// context, so at worst it seems a buggy or malicious emitter of logs
+// can cause problems for themselves only.
+func (c *MiniClient) BufferMessage(
+	when time.Time, procId string, log []byte) MiniStats {
+	// Avoid racing against other operations that may want to swap
+	// out client's current bundle.
+	c.bSwapLock.Lock()
+	defer c.bSwapLock.Unlock()
+
+	ts := when.UTC().Format(time.RFC3339)
+	syslogPrefix := "<134>1 " + ts + " 1234 " +
+		c.Token + " " + procId + " - - "
+	msgLen := len(syslogPrefix) + len(log)
+
+	fmt.Fprintf(&c.b.outbox, "%d %s%s", msgLen, syslogPrefix, log)
+	c.b.NumberFramed += 1
+	c.b.Buffered = c.b.outbox.Len()
+
+	return unsyncStats(c.b)
+}
+
+// Post messages that are pending being posted.
+func (c *MiniClient) PostMessages() (*http.Response, MiniStats, error) {
+	// Swap out the bundle that is about to go through a long I/O
+	// operation for a fresh one, so that buffering can continue
+	// again immediately.
+	b := func() bundle {
+		c.bSwapLock.Lock()
+		defer c.bSwapLock.Unlock()
+
+		b := *c.b
+		c.b.outbox = bytes.Buffer{}
+		c.b.MiniStats = MiniStats{}
+
+		return b
+	}()
+
+	// Record that a request is in progress so that a clean
+	// shutdown can wait for it to complete.
+	c.reqInFlight.Add(1)
+	defer c.reqInFlight.Done()
+
+	req, _ := http.NewRequest("POST", c.Logplex.String(), &b.outbox)
+	req.Header.Add("Content-Type", "application/logplex-1")
+	req.Header.Add("Logplex-Msg-Count",
+		strconv.FormatUint(b.NumberFramed, 10))
+
+	resp, err := c.HttpClient.Do(req)
+	if err != nil {
+		return nil, b.MiniStats, err
+	}
+
+	return resp, b.MiniStats, nil
+}
